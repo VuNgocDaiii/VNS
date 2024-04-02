d349973beda50c887fb4b4bdc59fcff9c980bb72
Vu Dai <vungocdaiii@gmail.com>
2024-04-02 21:00:06 +0700

    map fix

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,18 +2,21 @@ package com.example.myapplication;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
+    private SharedPreferences sharedPreferences;
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -24,10 +27,29 @@ public class MainActivity extends AppCompatActivity {
         webView = findViewById(R.id.webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.setWebViewClient(new WebViewClient());
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE);
         webView.addJavascriptInterface(new WebViewInterface(), "Android");
         webView.loadUrl("file:///android_asset/index.html");
+        WebView.setWebContentsDebuggingEnabled(true);
+        webView.getSettings().setAllowFileAccessFromFileURLs(true);
     }
+    private class JavaScriptInterface {
+        @JavascriptInterface
+        public void setLoggedInState(boolean isLoggedIn) {
+            // Lưu giá trị isLoggedIn vào SharedPreferences
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putBoolean("isLoggedIn", isLoggedIn);
+            editor.apply();
+        }
 
+        @JavascriptInterface
+        public boolean getLoggedInState() {
+            // Lấy giá trị isLoggedIn từ SharedPreferences
+            return sharedPreferences.getBoolean("isLoggedIn", false);
+        }
+
+        // Các phương thức khác (nếu có)
+    }
     // Phương thức để chuyển hướng đến trang apps.html
     public void loadAppsPage() {
         // chạy cùng luồng @@
