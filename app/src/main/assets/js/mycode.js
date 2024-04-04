a66eb68309a97badb357d6e83e2adc214eb151cf
ghun814 <hung080104@gmail.com>
2024-04-05 02:45:52 +0700

    Bill History

--- app/src/main/assets/js/mycode.js
+++ app/src/main/assets/js/mycode.js
@@ -5,3 +5,13 @@ function closeNav(){
 function openNav(){
     document.getElementById('mySidenav').style.width = '60%';
 }
+
+function closeLang(){
+    document.getElementById('lang').style.display = 'none';
+    document.getElementById('body').style.overflowY = 'unset';
+}
+
+function openLang(){
+    document.getElementById('lang').style.display = 'block';
+    document.getElementById('body').style.overflowY = 'hidden';
+}
